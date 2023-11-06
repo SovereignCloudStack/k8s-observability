2946b21f47bbd5144da74aa2c5f88f06589f57d0
Roman Hros <roman.hros@dnation.cloud>
2023-11-06 11:33:24 +0100

    Add kaas metric also to kaas.sh script
    Signed-off-by: Roman Hros <roman.hros@dnation.cloud>

--- mvp-0/kaas.sh
+++ mvp-0/kaas.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # Script creates (in parallel) multiple KinD clusters and bootstraps them with Prometheus-agent based monitoring stack
-# ./kaas.sh <action> <number of k8s clusters>
+# ./kaas.sh <action> <number of k8s clusters> <observer kubeconfig>
 
 if [ "$1" = "create" ]; then
   ACTION=$1
@@ -14,6 +14,10 @@ if ! [[ $2 =~ ^[0-9]+$ ]] || [[ -z "$2" ]] ; then
   echo "ERROR: Need to define number of KaaS clusters" >&2; exit 1
 fi
 
+if [[ -z "$3" ]] || ! [[ -f $3 ]] ; then
+  echo "ERROR: Need to provide observer kubeconfig" >&2; exit 1
+fi
+
 KAAS_NO=($(seq 1 1 "$2"))
 
 kaas_create() {
@@ -21,11 +25,15 @@ kaas_create() {
   echo "KaaS workload cluster kaas-$1 created"
   helm --kube-context kind-kaas-"$1" upgrade --install monitoring prometheus-community/kube-prometheus-stack -f values-workload.yaml --set prometheus.prometheusSpec.externalLabels.cluster=kaas-"$1"
   echo "KaaS workload cluster kaas-$1 bootstrapped with monitoring"
+  kubectl --kubeconfig "$3" patch cm kaas-clusters -p '{"data":{"'"kaas-$1"'":""}}'
+  echo "KaaS prometheus metric of workload cluster kaas-$1 imported"
 }
 
 kaas_delete() {
   kind delete cluster --name kaas-"$1"
   echo "KaaS workload cluster kaas-$1 deleted"
+  kubectl --kubeconfig "$3" patch cm kaas-clusters -p '{"data":{"'"kaas-$1"'":null}}'
+  echo "KaaS prometheus metric of workload cluster kaas-$1 removed"
 }
 
 export -f kaas_create
